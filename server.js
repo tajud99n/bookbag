ef2e4cb89e3ad11cff235e0a162e21d2b22eeb9a
tajud99n <giwa208@gmail.com>
2019-02-08 19:18:53 +0100

    POST /users route and token generation

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.get('/', (req, res) => {
     res.send({msg});
 });
 
+// Books Routes
 app.post('/books', (req, res) => {
     var book = new Book({
         title: req.body.title,
@@ -110,6 +111,20 @@ app.delete('/books/:id', (req, res) => {
         res.send({book});
     }).catch((err) => {
         res.status(400).send();
+    });
+});
+
+// Users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    
+    user.save().then(() => {
+        return user.generateAuthToken();       
+    }).then(token => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
     });
 });
 
