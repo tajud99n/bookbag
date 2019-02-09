37fee16792a0bf3e04e880f7de48a1eaebc90805
tajud99n <giwa208@gmail.com>
2019-02-09 14:57:19 +0100

    tests for POST /users endpoint

--- _test_/server.test.js
+++ _test_/server.test.js
@@ -4,144 +4,222 @@ const { ObjectID } = require("mongodb");
 
 const { app } = require("./../server");
 const Book = require("./../models/book");
-const {books, seedBooks, users, seedUsers} = require('./seed/seed');
-
+const User = require("./../models/user");
+const { books, seedBooks, users, seedUsers } = require("./seed/seed");
 
 beforeEach(seedUsers);
 beforeEach(seedBooks);
 
 describe("GET /", () => {
-    it("should response with a 200", done => {
-        request(app)
-            .get("/")
-            .expect(200)
-            .expect((res) => {
-                expect(res.body.msg).toBe("welcome");
-            })
-            .end(done);
-    });
+	it("should response with a 200", done => {
+		request(app)
+			.get("/")
+			.expect(200)
+			.expect(res => {
+				expect(res.body.msg).toBe("welcome");
+			})
+			.end(done);
+	});
 });
 
 describe("POST /books", () => {
-    it("should create a new book", done => {
-        const book = {
-            title: "book1",
-            author: "author1",
-            isbn: "12344",
-            rating: 3
-        };
+	it("should create a new book", done => {
+		const book = {
+			title: "book1",
+			author: "author1",
+			isbn: "12344",
+			rating: 3
+		};
 
-        request(app)
-            .post('/books')
-            .send(book)
-            .expect(200)
-            .expect(res => {
-                expect(res.body.title).toBe(book.title);
-            })
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                var { title } = book;
-                
-                Book.find({title})
-                    .then((result) => {
-                        expect(result.length).toBe(1);
-                        expect(result[0].title).toEqual(title);
-                        done();
-                    }).catch((err) => {
-                        done(err);
-                    });
-            });
-    });
+		request(app)
+			.post("/books")
+			.send(book)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.title).toBe(book.title);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				var { title } = book;
 
-    it("should not create a book if any of the required field is missing", done => {
-        request(app)
-            .post("/books")
-            .send({})
-            .expect(400)
-            .end(done);
-    });
+				Book.find({ title })
+					.then(result => {
+						expect(result.length).toBe(1);
+						expect(result[0].title).toEqual(title);
+						done();
+					})
+					.catch(err => {
+						done(err);
+					});
+			});
+	});
+
+	it("should not create a book if any of the required field is missing", done => {
+		request(app)
+			.post("/books")
+			.send({})
+			.expect(400)
+			.end(done);
+	});
 });
 
 describe("GET /books", () => {
-    it("should get all the todos", done => {
-        request(app)
-            .get("/books")
-            .expect(200)
-            .expect(res => {
-                expect(res.body.books.length).toBe(2);
-            })
-            .end(done);
-    });
+	it("should get all the todos", done => {
+		request(app)
+			.get("/books")
+			.expect(200)
+			.expect(res => {
+				expect(res.body.books.length).toBe(2);
+			})
+			.end(done);
+	});
 });
 
 describe("GET /books/:id", () => {
-    it("should return a book object", done => {
-        request(app)
-            .get(`/books/${books[1]._id.toHexString()}`)
-            .expect(200, done);
-    });
+	it("should return a book object", done => {
+		request(app)
+			.get(`/books/${books[1]._id.toHexString()}`)
+			.expect(200, done);
+	});
 
-    it("should return 404 if no book is found when passed a valid ObjectID that does not exist in the database", done => {
-        var bookID = new ObjectID().toHexString();
+	it("should return 404 if no book is found when passed a valid ObjectID that does not exist in the database", done => {
+		var bookID = new ObjectID().toHexString();
 
-        request(app)
-            .get(`/books/${bookID}`)
-            .expect(404)
-            .expect(res => {
-                expect(res.body.book).toBe(undefined);
-            })
-            .end(done);
-    });
+		request(app)
+			.get(`/books/${bookID}`)
+			.expect(404)
+			.expect(res => {
+				expect(res.body.book).toBe(undefined);
+			})
+			.end(done);
+	});
 
-    it("should return 404 for an invalid ObjectID", done => {
-        request(app)
-            .get("/books/122345")
-            .expect(404)
-            .expect(res => {
-                expect(res.body).toEqual({});
-            })
-            .end(done);
-    });
+	it("should return 404 for an invalid ObjectID", done => {
+		request(app)
+			.get("/books/122345")
+			.expect(404)
+			.expect(res => {
+				expect(res.body).toEqual({});
+			})
+			.end(done);
+	});
 });
 
 describe("PATCH /books/:id", () => {
-    it("should update a book record when provided with a valid ObjectID which exist in the database", done => {
-        var bookID = books[0]._id.toHexString();
-        var title = "test book";
+	it("should update a book record when provided with a valid ObjectID which exist in the database", done => {
+		var bookID = books[0]._id.toHexString();
+		var title = "test book";
 
-        request(app)
-            .patch(`/books/${bookID}`)
-            .send({ title })
-            .expect(200)
-            .expect(res => {
-                expect(res.body.book.title).toBe(title);
-            })
-            .end(done);
-    });
+		request(app)
+			.patch(`/books/${bookID}`)
+			.send({ title })
+			.expect(200)
+			.expect(res => {
+				expect(res.body.book.title).toBe(title);
+			})
+			.end(done);
+	});
 });
 
 describe("DELETE /books/:id", () => {
-    it("should delete a book", done => {
-        var bookID = books[0]._id.toHexString();
+	it("should delete a book", done => {
+		var bookID = books[0]._id.toHexString();
+
+		request(app)
+			.delete(`/books/${bookID}`)
+			.expect(200)
+			.expect(res => {
+				expect(res.body.book._id).toBe(bookID);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				Book.findById(res.body.book._id)
+					.then(book => {
+						expect(book).toBeNull();
+						done();
+					})
+					.catch(err => {
+						done();
+					});
+			});
+	});
+});
+
+describe("GET /users/me", () => {
+	it("should return user if authenticated", done => {
+		request(app)
+			.get("/users/me")
+			.set("x-auth", users[0].tokens[0].token)
+			.expect(200)
+			.expect(res => {
+				expect(res.body._id).toBe(users[0]._id.toHexString());
+				expect(res.body.email).toBe(users[0].email);
+			})
+			.end(done);
+	});
+
+	it("should return 401 if not authenticated", done => {
+		request(app)
+			.get("/users/me")
+			.expect(401)
+			.expect(res => {
+				expect(res.body).toEqual({});
+			})
+			.end(done);
+	});
+});
+
+describe("POST /users", () => {
+	it("should create a user", done => {
+		var email = "example@example.com";
+		var password = "password";
+
+		request(app)
+			.post("/users")
+			.send({ email, password })
+			.expect(200)
+			.expect(res => {
+				expect(res.headers["x-auth"]).toBeTruthy();
+				expect(res.body._id).toBeTruthy();
+				expect(res.body.email).toBe(email);
+			})
+			.end(err => {
+				if (err) {
+					return done(err);
+				}
+
+				User.findOne({ email })
+					.then(user => {
+						expect(user).toBeTruthy();
+						done();
+					})
+					.catch(err => done(err));
+			});
+	});
+
+	it("should return validation errors if request invalid", done => {
+		var email = 'example"example.com';
+		var password = "password";
+
+		request(app)
+			.post("/users")
+			.send({ email, password })
+			.expect(400)
+			.end(done);
+	});
+
+	it("should not create user if email in use", done => {
+        var email = "book@example.com";
+        var password = "password";
 
         request(app)
-            .delete(`/books/${bookID}`)
-            .expect(200)
-            .expect(res => {
-                expect(res.body.book._id).toBe(bookID);
-            })
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                };
-                Book.findById(res.body.book._id).then((book) => {
-                    expect(book).toBeNull();
-                    done();
-                }).catch((err) => {
-                    done();
-                });
-            });
-    });
+            .post('/users')
+            .send({ email, password })
+            .expect(400)
+            .end(done);
+	});
 });
